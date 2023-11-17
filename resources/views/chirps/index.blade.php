446a1bc307f42cb6c5515dbbee969e94327c4765
iamotuekong1 <iamotuekong1@gmail.com>
2023-11-17 22:22:07 +0100

    Index Blade SVG Icons Edit

--- resources/views/chirps/index.blade.php
+++ resources/views/chirps/index.blade.php
@@ -16,14 +16,14 @@
                 <div class="p-4 border-b">
                     <div class="flex items-center justify-between">
                         <div class="flex items-center space-x-2">
-                            <svg class="h-6 w-6 text-gray-600" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                                <!-- Your SVG icon -->
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-gray-600 -scale-x-100" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z" />
                             </svg>
                             <div>
                                 <span class="font-semibold">{{ $chirp->user->name }}</span>
                                 <small class="text-gray-600">{{ $chirp->created_at->format('j M Y, g:i a') }}</small>
                                 @unless ($chirp->created_at->eq($chirp->updated_at))
-                                    <small class="text-gray-600">{{ __('edited') }}</small>
+                                    <small class="text-sm text-gray-600"> &middot; {{ __('edited') }}</small>
                                 @endunless
                             </div>
                         </div>
@@ -31,8 +31,8 @@
                             <x-dropdown>
                                 <x-slot name="trigger">
                                     <button>
-                                        <svg class="h-4 w-4 text-gray-400" viewBox="0 0 20 20" fill="currentColor">
-                                            <!-- Your dropdown trigger icon -->
+                                        <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 text-gray-400" viewBox="0 0 20 20" fill="currentColor">
+                                            <path d="M6 10a2 2 0 11-4 0 2 2 0 014 0zM12 10a2 2 0 11-4 0 2 2 0 014 0zM16 12a2 2 0 100-4 2 2 0 000 4z" />
                                         </svg>
                                     </button>
                                 </x-slot>
